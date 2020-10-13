8845c3c562d02b3e83823ab4ae2e5becc5bfd817
Nisarg Patel <nisargmpatel73@gmail.com>
2020-10-13 19:49:08 +0530

    Update instructions for distinguishing between adv. and mains rank

--- frontend/src/constants/instructions.js
+++ frontend/src/constants/instructions.js
@@ -2,7 +2,7 @@ export default [
   'Every filter is applied as soon as you select its value and results can be seen upon scrolling to below table.',
   'Column headers can be clicked to sort any particular column in ascending/descending order.',
   'Opening/Closing Rank that have ( - P ) with it denotes Preparatory Rank.',
-  'Personal Rank based search: Enter your JEE Adv. Rank (n) and a delta (d), and you will get all the programs for which closing rank > (n + d)',
-  'Example 1: JEE Adv. Rank = 1000 and delta = 100; will give you all the programs whose closing rank is >1100',
-  'Example 2: JEE Adv. Rank = 1000 and delta = -50; will give you all the programs whose closing rank is >950'
+  'Personal Rank based search: Enter your JEE Adv. Rank or JEE Main Rank (n) and a delta (d), and you will get all the programs for which closing rank > (n + d)',
+  'Example 1: JEE Adv. Rank = 1000 and delta = 100; will give you all the programs in IITs whose closing rank is >1100',
+  'Example 2: JEE Main Rank = 1000 and delta = -50; will give you all the programs in NITs whose closing rank is >950'
 ]
